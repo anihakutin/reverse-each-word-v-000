4dc2819f12ac5b1cb2fc3d9a6c19a62e2584a6c4
Heshie Brody <4hbrody@gmail.com>
2021-04-25 03:37:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,8 @@
-# using 'each' method
-
-# def reverse_each_word(sentence)
-#   sentence = sentence.split
-#   reversed_words = []
-#   sentence.each do |word|
-#     reversed_words << "#{word.reverse}"
-#   end
-#     return reversed_words.join(" ")
-# end
-
-# using '.collect' method
-
 def reverse_each_word(sentence)
-  sentence = sentence.split
-  sentence.collect do |word|
-    word.reverse!
+  sentence_array = sentence.split(" ")
+  reversed_words_array = []
+  sentence_array.each do |word|
+    reversed_words_array << word.reverse
   end
-    return sentence.join(" ")
+  reversed_words_array
 end
